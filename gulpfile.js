00fdef355927dfc0a153773a6788c94cd8650120
Johnnie Munger <johnnie@wonderful.io>
2020-09-30 09:31:56 -0700

    adds imagemin task

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@
 // Importing specific gulp API functions lets us write them below as series() instead of gulp.series()
 const { src, dest, watch, series, parallel } = require('gulp');
 // Importing all the Gulp-related packages we want to use
+const changed = require('gulp-changed')
+const imagemin = require('gulp-imagemin');
 const sourcemaps = require('gulp-sourcemaps');
 const sass = require('gulp-sass');
 const concat = require('gulp-concat');
@@ -9,18 +11,18 @@ const uglify = require('gulp-uglify');
 const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
 const cssnano = require('cssnano');
-var replace = require('gulp-replace');
-
 
 // A dictionary of various directories
 const directories = {
 	dist : {
 		css : './css/',
+		images : './images/',
 		js : './js/',
 	},
 	src : {
-		js : './js/src/',
-		scss : './css/src/'
+		images : 'images/src/',
+		js : 'js/src/',
+		scss : 'css/src/'
 	}
 };
 
@@ -31,6 +33,15 @@ const files = {
 		scss : directories.src.scss + 'styles.scss'
 	}
 }
+
+// Image minification task: optimizes images for web and saves as a separate file
+function imageMinTask() {
+	return src(directories.src.images + "*")
+		.pipe(changed(directories.dist.images))
+		.pipe(imagemin())
+		.pipe(dest(directories.dist.images));
+}
+
 
 // JS task: concatenates and uglifies JS files to script.js
 function jsTask() {
@@ -57,20 +68,21 @@ function scssTask() {
 // If any change, run scss and js tasks simultaneously
 function watchTask() {
     watch([
+	    	directories.src.images + '**/*.*', 
 	    	directories.src.scss + '**/*.scss', 
 	    	directories.src.js + '**/*.js'
     	],
         {interval: 1000, usePolling: true}, //Makes docker work
         series(
-            parallel(scssTask, jsTask)
+            parallel(imageMinTask, scssTask, jsTask)
         )
     );    
 }
 
 // Export the default Gulp task so it can be run
-// Runs the scss and js tasks simultaneously
+// Runs the images, scss and js tasks simultaneously
 // then watch task
 exports.default = series(
-    parallel(scssTask,jsTask),
+    parallel(imageMinTask, scssTask, jsTask),
     watchTask
 );
